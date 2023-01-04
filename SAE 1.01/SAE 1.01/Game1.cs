47e930289c8c40df1f7da39275e2db070a224146
Ethan Rusafa <ethan.rusafa@etu.univ-smb.fr>
2023-01-04 10:40:06 +0100

    creation map

--- SAE 1.01/SAE 1.01/Game1.cs
+++ SAE 1.01/SAE 1.01/Game1.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace SAE_1._01
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
+        public GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        MapCreator _map01;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,14 +22,15 @@ namespace SAE_1._01
         {
             // TODO: Add your initialization logic here
             Window.Title ="Test" ;
+            _map01 = new MapCreator("mapaTest", this);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,7 +40,7 @@ namespace SAE_1._01
             float terre = 2;
 
             // TODO: Add your update logic here
-
+            _map01.UpdateMap(gameTime);
             base.Update(gameTime);
         }
 
@@ -47,7 +50,7 @@ namespace SAE_1._01
             GraphicsDevice.Clear(Color.Red);
 
             // TODO: Add your drawing code here
-
+            _map01.DrawMap();
             base.Draw(gameTime);
         }
     }
---
+++ SAE 1.01/SAE 1.01/MapCreator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SAE_1._01
+{
+    internal class MapCreator : Game
+{
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
+        private GraphicsDevice _graphics;
+        private GraphicsDeviceManager _graphicsManager;
+
+        public MapCreator(string nomMap, Game1 game1)
+        {
+            Content = game1.Content;
+            _graphics = game1.GraphicsDevice;
+            _graphics.BlendState = BlendState.AlphaBlend;
+            _graphicsManager = game1._graphics;
+            GetContent(nomMap);
+            
+            
+        }
+
+        void GetContent(string nomMap)
+        {
+            _tiledMap = Content.Load<TiledMap>(nomMap);
+            _tiledMapRenderer = new TiledMapRenderer(_graphics, _tiledMap);
+        }
+
+        public void DrawMap()
+        {
+            _tiledMapRenderer.Draw();
+        }
+
+        public void UpdateMap(GameTime gt)
+        {
+            _tiledMapRenderer.Update(gt);
+        }
+
+        public Vector2 GetMapSize()
+        {
+            return new Vector2(_tiledMap.WidthInPixels, _tiledMap.HeightInPixels);
+        }
+
+        void SetWindowToMapSize()
+        {
+            Vector2 size = this.GetMapSize();
+            _graphicsManager.PreferredBackBufferWidth = (int)size.X;
+            _graphicsManager.PreferredBackBufferHeight = (int)size.Y;
+            _graphicsManager.ApplyChanges();
+        }
+
+      
+      
+
+    }
+}
