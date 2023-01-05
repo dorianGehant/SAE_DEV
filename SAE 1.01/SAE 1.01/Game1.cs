0c8448371efc5a079bea1ee87debdcadde923945
Ethan Rusafa <ethan.rusafa@etu.univ-smb.fr>
2023-01-05 14:19:19 +0100

    joueur animation + deplcamenet

--- SAE 1.01/SAE 1.01/Game1.cs
+++ SAE 1.01/SAE 1.01/Game1.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using Input;
+using System.Diagnostics;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Serialization;
 
 namespace SAE_1._01
 {
     public class Game1 : Game
     {
-
+        
+        const int LONGUEUR_CASE = 19, HAUTEUR_CASE = 13,TAILLE_CASE = 25;
         public GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private MouseState _etatSouris;
@@ -24,7 +31,10 @@ namespace SAE_1._01
 
         private Joueur _joueur1;
         private Texture2D _texturePersonnage;
-
+        private Texture2D _textureSelectionne;
+        Case selectionne;
+        Carte cases;
+        Joueur j1;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -34,53 +44,70 @@ namespace SAE_1._01
 
         protected override void Initialize()
         {
-
+            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-
-            _map01 = new CreateurCarte("mapaTest", this);
+            
+            //valeur des tailles 
             _longueurCase = (int)_map01.TailleCase().X;
             _hauteurCase = (int)_map01.TailleCase().Y;
             _nbLignesCarte = (int)_map01.TailleCarte().Y / _hauteurCase;
             _nbColonnesCarte = (int)_map01.TailleCarte().X / _longueurCase;
 
 
+            //On load les differents elements
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
             _bordureCase = Content.Load<Texture2D>("contour_case");
+            _textureSelectionne = Content.Load<Texture2D>("New Piskel-1");
+            _texturePersonnage = Content.Load<Texture2D>("perso");
 
-            _tableauCases = new Case[_nbLignesCarte, _nbColonnesCarte];
-            for (int i = 0; i < _nbLignesCarte; i++)
-            {
-                for (int j = 0; j < _nbColonnesCarte; j++)
-                {
-                    _tableauCases[i, j] = new Case(j, i, _bordureCase);
-                }
-            }
+            //creation des objets utiles
+            selectionne = new Case(-100, -100, _textureSelectionne);
+            _map01 = new CreateurCarte("mapaTest", this);
+            j1 = new Joueur(spriteSheet, "j1", cases.TableauCases[0, 0], 1, 1);
+            cases = new Carte(LONGUEUR_CASE, HAUTEUR_CASE, TAILLE_CASE, _bordureCase);
 
-            _carte = new Carte(_nbColonnesCarte, _nbLignesCarte, _tableauCases);
 
-            _texturePersonnage = Content.Load<Texture2D>("characters");
-            _joueur1 = new Joueur(_texturePersonnage, "J1", _carte.TableauCases[5,5], 25, 3);
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            //les inputs
+            var simulation = Inputs.Use<IMouseSimulation>();
             _etatClavier = Keyboard.GetState();
             _etatSouris = Mouse.GetState();
 
-            if (_etatSouris.LeftButton == ButtonState.Pressed)
-            {
-                _joueur1.Deplacer(new Vector2(_etatSouris.Position.X/_longueurCase, _etatSouris.Position.Y / _hauteurCase), _carte);
-            }
+            //positions souris
+            int x = (_etatSouris.X - Window.Position.X) / TAILLE_CASE;
+            int y = (_etatSouris.Y + - Window.Position.Y) / TAILLE_CASE;
 
+            //on verifie si la souris se trouve bien sur une case
+            if(x >= 0 && x < LONGUEUR_CASE && y >= 0 && y < HAUTEUR_CASE)
+            {
+                //on met le carre bleu sur la case ou il y a la souris
+                selectionne.X = x * TAILLE_CASE;
+                selectionne.Y = y * TAILLE_CASE;
+
+                //verification clique
+                if (simulation.IsMouseDown(InputMouseButtons.Left))
+                {
+                    //On bouge le joueur ou on clique
+                    j1.MovePlayer(cases.TableauCases[x,y]);
+                }
+            }    
+
+            //mise a jour / update
+            j1.UpdateAnim(deltaSeconds);
             _map01.MiseAJour(gameTime);
+
             base.Update(gameTime);
         }
 
@@ -89,16 +116,17 @@ namespace SAE_1._01
             GraphicsDevice.Clear(Color.Red);
 
             // TODO: Add your drawing code here
-            _map01.Dessiner();
             _spriteBatch.Begin();
-            foreach (Case casecarte in _carte.TableauCases)
-            {
-                _spriteBatch.Draw(casecarte.Texture, new Vector2(casecarte.X * _longueurCase, casecarte.Y * _hauteurCase), Color.White);
-            }
-            _spriteBatch.Draw(_joueur1.Texture, new Vector2((_joueur1.Position.X * _longueurCase) - _joueur1.Texture.Width/2, (_joueur1.Position.Y * _hauteurCase) - _joueur1.Texture.Height/2), Color.White);
+            _map01.Dessiner();
+            cases.AfficherMap(_spriteBatch);
+            _spriteBatch.Draw(selectionne.Texture, new Vector2(selectionne.X, selectionne.Y), Color.White);
+            j1.Afficher(_spriteBatch);
             _spriteBatch.End();
             
             base.Draw(gameTime);
         }
+
+
+
     }
 }
--- SAE 1.01/SAE 1.01/Joueur.cs
+++ SAE 1.01/SAE 1.01/Joueur.cs
@@ -5,17 +5,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonoGame.Extended.Sprites;
 
 namespace SAE_1._01
 {
     internal class Joueur : Entite
     {
         private List<Sort> sorts;
-
-        public Joueur(Texture2D texture, string nom, Case position, int pointVie, int pointAction)
-            : base(texture, nom, position, pointVie, pointAction)
+        public AnimatedSprite sprite;
+        public Joueur(SpriteSheet spritesheet, string nom, Case position, int pointVie, int pointAction)
+            : base(spritesheet, nom, position, pointVie, pointAction)
         {
+            sprite = new AnimatedSprite(spritesheet);
+            this.PlayAnim("attaque");
             //this.sorts = new List<Sort>(sorts);
+        }
+
+        public void MovePlayer(Case c)
+        {
+            this.Position = c;
+        }
+
+        public void Afficher(SpriteBatch batch)
+        {
+            batch.Draw(this.sprite, new Vector2(Position.X, Position.Y));
+        }
+
+        public void PlayAnim(string anim)
+        {
+            sprite.Play(anim);
+        }
+
+        public void UpdateAnim(float deltaseconds)
+        {
+            sprite.Update(deltaseconds);
         }
     }
 }
